9acc2e8b623cb5132c7e5febecb62e6edf330a0a
Craig Dennis <craigmdennis@gmail.com>
2014-05-24 00:47:36 +0100

    feat: add email form

---
+++ dist/email.php
@@ -0,0 +1,17 @@
+<?php
+    $name    = $_REQUEST['name'];
+    $email   = $_REQUEST['email'];
+    $message = $_REQUEST['message'];
+
+    if (($name=="")||($email=="")||($message=="")) {
+      echo "All fields are required, please fill <a href=\"\">the form</a> again.";
+    }
+
+    else {
+      $from    = "From: $name<$email>\r\nReturn-path: $email";
+      $subject = "Possible lead";
+      mail("info@simplebitdesign.com", $subject, $message, $from);
+      header( 'Location: http://simplebitdesign.com/thankyou.html' );
+    }
+
+?>
