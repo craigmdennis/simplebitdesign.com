80fb1b21c07aac260494c4e56b5d7a5387e4671f
Craig Dennis <craigmdennis@gmail.com>
2014-06-19 03:17:04 -0700

    chore: add deploy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -483,11 +483,11 @@ module.exports = function (grunt) {
     'htmlmin'
   ]);
 
-  grunt.registerTask('stage', [
+  grunt.registerTask('deploy', [
     'bump-only:minor',
     'changelog',
     'bump-commit',
-    'shell:staging'
+    'shell:prod'
   ]);
 
 };
