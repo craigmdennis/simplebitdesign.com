f90fb9895b931b8c7e86b147cb7639991295d7b3
Craig Dennis <craigmdennis@gmail.com>
2016-05-03 15:39:28 +0100

    Remove things that can't be live. Fix issues with fullheight + typos

--- gulpfile.js/tasks/css.js
+++ gulpfile.js/tasks/css.js
@@ -1,16 +1,17 @@
 var config       = require('../config')
 if(!config.tasks.css) return
 
-var gulp         = require('gulp')
-var gulpif       = require('gulp-if')
-var browserSync  = require('browser-sync')
-var sass         = require('gulp-sass')
-var sourcemaps   = require('gulp-sourcemaps')
-var handleErrors = require('../lib/handleErrors')
-var autoprefixer = require('gulp-autoprefixer')
-var path         = require('path')
-var cssnano      = require('gulp-cssnano')
-var cssimport    = require("gulp-cssimport")
+var gulp            = require('gulp')
+var gulpif          = require('gulp-if')
+var browserSync     = require('browser-sync')
+var sass            = require('gulp-sass')
+var sourcemaps      = require('gulp-sourcemaps')
+var handleErrors    = require('../lib/handleErrors')
+var path            = require('path')
+var postcss         = require('gulp-postcss')
+var cssimport       = require('postcss-import')
+var autoprefixer    = require('autoprefixer')
+var cssnano         = require('cssnano')
 
 var paths = {
   src: path.join(config.root.src, config.tasks.css.src, '/**/*.{' + config.tasks.css.extensions + '}'),
@@ -18,14 +19,17 @@ var paths = {
 }
 
 var cssTask = function () {
+  var processors = [
+    cssimport(),
+    autoprefixer({browsers: ['last 2 versions']}),
+    cssnano()
+  ];
   return gulp.src(paths.src)
     .pipe(gulpif(!global.production, sourcemaps.init()))
-    .pipe(sass(config.tasks.css.sass))
+    .pipe(sass(config.tasks.css.sass)
+    .on('error', handleErrors))
+    .pipe(postcss(processors))
     .on('error', handleErrors)
-    .pipe(cssimport())
-    .on('error', handleErrors)
-    .pipe(autoprefixer(config.tasks.css.autoprefixer))
-    .pipe(gulpif(global.production, cssnano({autoprefixer: false})))
     .pipe(gulpif(!global.production, sourcemaps.write()))
     .pipe(gulp.dest(paths.dest))
     .pipe(browserSync.stream())
